2e11742205e4c4c4a1701776fbafee735c1c1b92
Kunitaka Ishii <ishii@iret.co.jp>
2023-03-29 17:05:11 +0900

    React Router setting

--- resources/ts/components/Example.tsx
+++ resources/ts/components/Example.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Routes from '../routes'
 
 function Example() {
     return (
@@ -25,7 +26,7 @@ if (document.getElementById('app')) {
 
     Index.render(
         <React.StrictMode>
-            <Example/>
+            <Routes />
         </React.StrictMode>
     )
 }
